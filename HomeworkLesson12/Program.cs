5e4405e2cdea6c4b208cf3e9b37065de78b0f774
Nasiba <nasyrova.nasiba@gmail.com>
2021-02-19 20:55:30 +0500

    init

---
+++ HomeworkLesson12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HomeworkLesson12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
